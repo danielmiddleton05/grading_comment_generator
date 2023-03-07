2144e528546bb8eb09e9c4b3e4c1fa39dae44252
danielmiddleton05 <119559669+danielmiddleton05@users.noreply.github.com>
2023-03-07 13:28:17 +0900

    updated the jv code

--- js/scripts.js
+++ js/scripts.js
@@ -1,122 +1,56 @@
-function showText1() {
-    var userInput = document.getElementById("userInput1").value;
-    var outputText = "";
-    switch (userInput) {
-        case "1":
-            outputText = "The essay has a discussion is weakly connected to the title and";
-            break;
-        case "2":
-            outputText = "The essay has a discussion is weakly connected to the title and";
-            break;
-        case "3":
-            outputText = "The essay has a discussion is connected to the title and";
-            break;
-        case "4":
-            outputText = "The essay has a discussion is connected to the title and";
-            break;
-        case "5":
-            outputText = "The essay has a discussion is mostly focused on the title and";
-            break;
-        case "6":
-            outputText = "The essay has a discussion is mostly focused on the title and";
-            break;
-        case "7":
-            outputText = "The essay has a discussion is focused on the title and";
-            break;
-        case "8":
-            outputText = "The essay has a discussion is focused on the title and";
-            break;
-        case "9":
-            outputText = "The essay has a discussion has a sustained focus on the title and";
-            break;
-        case "10":
-            outputText = "The essay has a discussion has a sustained focus on the title and";
-            break;
-        default:
-            outputText = "Please enter a number between 1 and 10!";
-            break;
-    }
+const criteria1 = {
+    "1": "The essay has a discussion that is weakly connected to the title and",
+    "2": "The essay has a discussion that is weakly connected to the title and",
+    "3": "The essay has a discussion is connected to the title and",
+    "4": "The essay has a discussion is connected to the title and",
+    "5": "The essay has a discussion is mostly focused on the title and",
+    "6": "The essay has a discussion is mostly focused on the title and",
+    "7": "The essay has a discussion is focused on the title and",
+    "8": "The essay has a discussion is focused on the title and",
+    "9": "The essay has a discussion has a sustained focus on the title and",
+    "10": "The essay has a discussion has a sustained focus on the title and",
+  };
+  
+  const criteria2 = {
+    "1": "links to AOKs but is not relevant.",
+    "2": "links to AOKs but is not relevant.",
+    "3": "makes superficial or limited links to the AOKs.",
+    "4": "makes superficial or limited links to the AOKs.",
+    "5": "developed with some links to the AOKs.",
+    "6": "developed with some links to the AOKs.",
+    "7": "mostly linked effectively to the AOKs.",
+    "8": "mostly linked effectively to the AOKs.",
+    "9": "linked effectively to the AOKs.",
+    "10": "linked effectively to the AOKs.",
+  };
+  
+  const criteria3 = {
+    "1": "Mostly descriptive with little arguments",
+    "2": "Mostly descriptive with little arguments",
+    "3": "Limited arguments are offered",
+    "4": "Limited arguments are offered",
+    "5": "General arguments are offered",
+    "6": "General arguments are offered",
+    "7": "Arguments are clear, coherent",
+    "8": "Arguments are clear, coherent",
+    "9": "Arguments are clear, coherent, and convincing",
+    "10": "Arguments are clear, coherent, and convincing",
+  };
+  
+  function showText1() {
+    const userInput = document.getElementById("userInput1").value;
+    const outputText = criteria1[userInput] || "Please enter a number between 1 and 10!";
     document.getElementById("output1").innerHTML = outputText;
-}
-
-function showText2() {
-    var userInput = document.getElementById("userInput2").value;
-    var outputText = "";
-    switch (userInput) {
-        case "1":
-            outputText = "links to AOKs but is not relevant.";
-            break;
-        case "2":
-            outputText = "links to AOKs but is not relevant.";
-            break;
-        case "3":
-            outputText = "makes superficial or limited links to the AOKs.";
-            break;
-        case "4":
-            outputText = "makes superficial or limited links to the AOKs.";
-            break;
-        case "5":
-            outputText = "developed with some links to the AOKs.";
-            break;
-        case "6":
-            outputText = "developed with some links to the AOKs.";
-            break;
-        case "7":
-            outputText = "mostly linked effectively to the AOKs.";
-            break;
-        case "8":
-            outputText = "mostly linked effectively to the AOKs.";
-            break;
-        case "9":
-            outputText = "linked effectively to the AOKs.";
-            break;
-        case "10":
-            outputText = "linked effectively to the AOKs.";
-            break;
-        default:
-            outputText = "Please enter a number between 1 and 10!";
-            break;
-    }
+  }
+  
+  function showText2() {
+    const userInput = document.getElementById("userInput2").value;
+    const outputText = criteria2[userInput] || "Please enter a number between 1 and 10!";
     document.getElementById("output2").innerHTML = outputText;
-}
-
-function showText3() {
-    var userInput = document.getElementById("userInput3").value;
-    var outputText = "";
-    switch (userInput) {
-        case "1":
-            outputText = "Mostly descriptive with little arguments";
-            break;
-        case "2":
-            outputText = "Mostly descriptive with little arguments";
-            break;
-        case "3":
-            outputText = "Limited arguments are offered";
-            break;
-        case "4":
-            outputText = "Limited arguments are offered";
-            break;
-        case "5":
-            outputText = "Generic arguments are offered";
-            break;
-        case "6":
-            outputText = "Generic arguments are offered";
-            break;
-        case "7":
-            outputText = "Arguments are clear, coherent";
-            break;
-        case "8":
-            outputText = "Arguments are clear, coherent";
-            break;
-        case "9":
-            outputText = "Arguments are clear, coherent, and convincing";
-            break;
-        case "10":
-            outputText = "Arguments are clear, coherent, and convincing";
-            break;
-        default:
-            outputText = "Please enter a number between 1 and 10!";
-            break;
-    }
+  }
+  
+  function showText3() {
+    const userInput = document.getElementById("userInput3").value;
+    const outputText = criteria3[userInput] || "Please enter a number between 1 and 10!";
     document.getElementById("output3").innerHTML = outputText;
-}
+  }
